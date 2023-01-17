8503833c88b5142e828ff05cb50fc10f22b2c44f
John Cochran <john@Johns-Mac-mini.local>
2023-01-16 20:04:23 -0700

    Initial Commit for the Elixir Matroid Library
     Committer: John Cochran <john@Johns-Mac-mini.local>
    
     On branch main
    
     Initial commit
    
     Changes to be committed:
    	new file:   .formatter.exs
    	new file:   .gitignore
    	new file:   README.md
    	new file:   lib/matroid.ex
    	new file:   mix.exs
    	new file:   test/matroid_test.exs
    	new file:   test/test_helper.exs

---
+++ lib/matroid.ex
@@ -0,0 +1,18 @@
+defmodule Matroid do
+  @moduledoc """
+  Documentation for `Matroid`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Matroid.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/matroid_test.exs
@@ -0,0 +1,8 @@
+defmodule MatroidTest do
+  use ExUnit.Case
+  doctest Matroid
+
+  test "greets the world" do
+    assert Matroid.hello() == :world
+  end
+end
