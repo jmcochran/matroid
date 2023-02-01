228c469f6b8aca5e872df1d153f72804ccd5eed3
John Cochran <jmcochran65@gmail.com>
2023-02-01 15:23:04 -0700

    Worked on the BasesMatroid module and deleted the IndependenceMatroid module.
     On branch create_base_matroid_protocol
     Changes to be committed:
    	modified:   lib/bases_matroid.ex
    	deleted:    lib/independence_matroid.ex

--- lib/bases_matroid.ex
+++ lib/bases_matroid.ex
@@ -6,17 +6,23 @@ defmodule BasesMatroid do
   defimpl Matroid do
     # TODO: fix this implementation
     @spec includes?(%BasesMatroid{}, %MapSet{}) :: boolean
-    def includes?(%BasesMatroid{ground_set: _gs, bases: bs}, set), do: set in bs
+    def includes?(%BasesMatroid{ground_set: _gs, bases: bs}, set) do
+      bs |> Enum.reduce(false, fn (b, acc) -> acc or (set |> MapSet.subset?(MapSet.new(b))) end)
+    end
     @spec ground_set(%BasesMatroid{}) :: any
     def ground_set(%BasesMatroid{ground_set: gs, bases: _bs}), do: gs
   end
 
-  @spec new(nonempty_list(%MapSet{}), nonempty_list(%MapSet{})) :: %BasesMatroid{
-          ground_set: nonempty_list(%MapSet{}),
-          bases: nonempty_list(%MapSet{})
-        }
-  def new(gs, bs) when is_list(gs) and is_list(bs) do
-    # TODO: add validations
-    %BasesMatroid{ground_set: gs, bases: bs}
+
+  @spec new(nonempty_list(any), nonempty_list(list(any))) ::
+          {:error} | {:ok, %BasesMatroid{bases: MapSet.t(), ground_set: MapSet.t()}}
+  def new(gs, bs) when length(gs) > 0 and length(bs) > 0 do
+    with gs_set <- MapSet.new(gs),
+         bs_set <- MapSet.new(bs |> Enum.map(fn b -> MapSet.new(b) end))
+    do
+      {:ok, %BasesMatroid{ground_set: gs_set, bases: bs_set}}
+    else
+      _ -> {:error}
+    end
   end
 end
